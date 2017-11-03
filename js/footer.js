b8f3ece404077040394fea648ece7e2bf1d358ee
Kenyan Burnham <kenyan.burnham@ttu.edu>
2017-11-03 11:54:28 -0500

    Trying to change header and footer

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+    return {
+        height: '2cm',
+        contents: '<div style="text-align: right;">Texas Tech University</div>'
+    };
+};
---
+++ js/header.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+    return {
+        height: '2cm',
+        contents: '<div style="text-align: right;">Program for Semiconductor Product Engineers</div>'
+    };
+};
