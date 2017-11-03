17473d292a38527ed63d4df8877cb026923ad280
Kenyan Burnham <kenyan.burnham@ttu.edu>
2017-11-03 12:30:27 -0500

    Still working with the header and footer

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-    return {
-        height: '2cm',
-        contents: '<div style="text-align: right;">Texas Tech University</div>'
-    };
-};
--- js/header.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-    return {
-        height: '2cm',
-        contents: '<div style="text-align: right;">Program for Semiconductor Product Engineers</div>'
-    };
-};
