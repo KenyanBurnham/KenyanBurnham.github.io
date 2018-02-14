cc0c1874cd34ee0be5b454fcc8a0a8e4da7e6533
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-02-14 14:37:49 -0600

    Adding more documentation

--- js/test.js
+++ js/test.js
@@ -1,3 +1,9 @@
+/*
+Developed by Kenyan Burnham
+For: the Program for Semiconductor Product Engineering Testing Laboratory
+Description: 
+*/
+
 //Whetever the current time is should be highlighted in the left column by changing the css of "the-hour siding" to highlight the cell
 // Will have to add a reservation input form
 // Will have to add to db
