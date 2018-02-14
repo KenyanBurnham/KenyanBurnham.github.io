185d3021ce3ed3a2f7701d40d291a735f4cc5e7c
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-02-14 14:23:36 -0600

    seeing what kind of variable this creates

--- js/test.js
+++ js/test.js
@@ -14,7 +14,7 @@ $(function(){
 
         // Gets class of object and logs the first and second class
         var classList = $(this).attr('class').split(/\s+/);
-        reservation.add[classList[1] + " " + classList[2]];
-        console.log(reservation);
+        var addReservation = classList[1] + " " + classList[2]
+        console.log(addReservation);
     });
 });
