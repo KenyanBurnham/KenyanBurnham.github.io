8f6bc336ebd4b218ed5c597804db0b6ec65da22d
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-06-15 11:53:02 -0500

    More number conversions?

--- js/reserve.js
+++ js/reserve.js
@@ -39,9 +39,8 @@ listener function:
 ===============================================================================*/
 function listener(){
     let timeStamp = $('.is-selected').attr("data-timestamp");
-    console.log(timeStamp);
     for(let i = 0; i<18; i++){
-        let newTimeStamp = (Number(timeStamp) + (3600*8) + (1800*i));
+        let newTimeStamp = (Number(timeStamp) + (3600*8) + Number(1800*i));
         $('td' + i + '').attr("name", newTimeStamp);
         console.log(newTimeStamp);
         console.log($('td' + i + '').attr("name"));
