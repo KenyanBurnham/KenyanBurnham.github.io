66cee68bb1b2ce06003d32d5998643735851f49d
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-06-15 13:36:51 -0500

    Adding css while I wait

--- js/reserve.js
+++ js/reserve.js
@@ -43,6 +43,7 @@ function listener(){
         let newTimeStamp = (Number(timeStamp) + (3600*8) + Number(1800*i));
         let tdID = 'td' + i;
         $(tdID).attr('data-timestamp', newTimeStamp);
+        $(tdID).css("background-color", "#7fcbc3");
         console.log($(tdID).attr("data-timestamp"));
     }
     let path = "requests/reservation/approved/" + timeStamp;
