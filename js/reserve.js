f65fd905f58e807fa681e586afe719e0bdd9045c
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-02 17:50:40 -0500

    Wonder when that happened

--- js/reserve.js
+++ js/reserve.js
@@ -19,6 +19,7 @@ function fillTable(selector){
         //filterPending(newTimeStamp);
         //filterApproved(newTimeStamp);
     }
+  }
 
 /*=============================================================================
 cleanUpDate function:
