a8f1a4566364420050d683f37ee5a0eb033ea151
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-02 14:31:22 -0500

    I do dislike how long it takes

---
+++ hello-week-master/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: "../dist/[name].min.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry : {
+        helloweek: './src/scripts/hello-week.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: '[name].min.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                minimize: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js']
+    },
+    plugins: [
+        extractSass
+    ]
+};
--- js/reserve.js
+++ js/reserve.js
@@ -1,5 +1,50 @@
-// /https://bootstrap-datepicker.readthedocs.io/en/latest/
-$('#selectDate').datepicker({
-    todayBtn: "linked",
-    daysOfWeekDisabled: "0,6"
+
+
+function doSomething(){
+    console.log("yeah");
+}
+
+/*=============================================================================
+cleanUpDate function:
+1. primitive way of formating the date to be correct for the calendar
+===============================================================================*/
+function cleanUpDate(){
+      let today = new Date().getDate();
+      today = today + 1;
+      let month = new Date().getMonth();
+      month = month + 1;
+      let year = new Date().getFullYear();
+      if(month <= 9){
+          month = "0" + month;
+      }
+      if(today <= 9){
+          today = "0" + today;
+      }
+      let todayDateDefault = year + "-" + month + "-" + today;
+      return todayDateDefault;
+}
+
+/*=============================================================================
+required header for hello-week:
+1. sets the options for the calendar
+Source: https://maurovieirareis.github.io/hello-week/demos/documentation.html
+Notes: All epoch times for days start at 12:00 AM
+===============================================================================*/
+new HelloWeek({
+    langFolder: 'hello-week-master/dist/langs/',
+    lang: 'en',
+    format: 'DD-MM-YYYY',
+    defaultDate: cleanUpDate(), // Only format YYYY-MM-DD
+    multiplePick: false,
+    daysHighlight: [],
+    todayHighlight: true,
+    disableDates: false,
+    disablePastDays: true,
+    disabledDaysOfWeek: [0,6],
+    weekStart: 0,
+    range: false,
+    onLoad: () => { /** callback function */ },
+    onChange: () => { /** callback function */ },
+    onSelect: () => { doSomething(); },
+    onClear: () => { /** callback function */ },
 });
