dc502c1602ccc1faa3ee3f9aa4fec7000b15bacb
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-03 10:52:24 -0500

    Ah, there's  the issue

--- js/reserve.js
+++ js/reserve.js
@@ -1,5 +1,5 @@
 function humanReadableTime(hour, minute){
-    let timesWhole = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM","4:00 PM"];
+    let timeWhole = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM","4:00 PM"];
     let timeHalf = ["9:30 AM", "10:30 AM", "11:30 AM", "12:30 PM", "1:30 PM", "2:30 PM", "3:30 PM", "4:30 PM"];
     console.log(hour);
     console.log(minute);
