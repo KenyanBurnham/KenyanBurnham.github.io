b2f44777e3afb3eff65717d87f22885722ef25a6
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-06 11:25:17 -0500

    Alright, added I think a better switch by Kenneth Sim

--- js/reserve.js
+++ js/reserve.js
@@ -143,5 +143,31 @@ new HelloWeek({
 });
 
 $( document ).ready(function() {
+      $('.btn-toggle').click(function() {
+          $(this).find('.btn').toggleClass('active');
 
+          if ($(this).find('.btn-primary').length>0) {
+              $(this).find('.btn').toggleClass('btn-primary');
+          }
+          if ($(this).find('.btn-danger').length>0) {
+              $(this).find('.btn').toggleClass('btn-danger');
+          }
+          if ($(this).find('.btn-success').length>0) {
+              $(this).find('.btn').toggleClass('btn-success');
+          }
+          if ($(this).find('.btn-info').length>0) {
+              $(this).find('.btn').toggleClass('btn-info');
+          }
+
+          $(this).find('.btn').toggleClass('btn-default');
+
+    });
+
+    $('form').submit(function(){
+        let radioValue = $("input[name='options']:checked").val();
+        if(radioValue){
+            console.log("You selected - " + radioValue);
+         };
+            return false;
+    });
 });
