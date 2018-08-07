c8bef56c689a95a791b148f1d2fa54ed980eab33
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-07 15:25:07 -0500

    Waiting to see if it works

--- js/reserve.js
+++ js/reserve.js
@@ -17,8 +17,8 @@ function equipmentList(){
               } else {
                   $("#optgroup" + equipmentData.sku).append(newEquipment);
               }
-              $("<option value='" + equipment.key + "'>").click(function(event){
-                  event.preventDefault();
+              $("<option value='" + equipment.key + "'>").click(function(){
+                  console.log(equipment.key);
                   let selected = "<a href='#' id='showing" + equipment.key + "' class='list-group-item list-group-item-action flex-column align-items-start'>" +
                   "<div class='d-flex w-100 justify-content-between'>" +"<small onclick='removeEquipment(" + equipment.key + ")'>X/small>" + "</div>" +
                   "<p class='mb-1'>" + equipmentData.manufacturer + equipmentData.model + "</p>" +
