7d1ef6d0d470a9669456c1056ba0085c68f03f2a
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-08 15:40:58 -0500

    Adding more inventory page stuff

--- js/equipment.js
+++ js/equipment.js
@@ -9,21 +9,34 @@ function generatePage(){
             let type = itemData.type;
             let sku = itemData.sku;
             let url = itemData.url;
+            let gpib = itemData.gpib;
             let skuCheck = $("#" + sku).hasClass("sku");
             if(skuCheck == false){
                 $("#bookmarkLinks").empty();
                 let bookmark = "<li><a href='#" + sku +"' class='make-block-item nav-sub'>" + type + "</a></li>"
                 $("#bookmarkLinks").append(bookmark);
-                let newRow = "<div class='row'><div class='col-2'></div><div class='col-2'>" +
-                      "<h4 id = '" + sku + "' class = 'title'>" + type + "</h4><ul id='ul" + sku + "'>" +
-                          "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + model + "</a></li>" +
-                        "</ul></div><div class='col-2'></div></div>";
-                //build section
-                //build equipment list item
-                //append to sku type
+                if(gpib == false){
+                    let newRow = "<div class='row'><div class='col-2'></div><div class='col-2'>" +
+                          "<h4 id = '" + sku + "' class = 'title'>" + type + "</h4><ul id='ul" + sku + "'>" +
+                              "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + model + "</a></li>" +
+                            "</ul></div><div class='col-2'></div></div>";
+                }
+                if(gpib == true){
+                    let newRow = "<div class='row'><div class='col-2'></div><div class='col-2'>" +
+                          "<h4 id = '" + sku + "' class = 'title'>" + type + "</h4><ul id='ul" + sku + "'>" +
+                              "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + model + "</a>" + span + "</li>" +
+                            "</ul></div><div class='col-2'></div></div>";
+                }
+                $("#benchTopEquipment").append(newRow);
             }else{
-                //build equipment list item
-                //append to sku type
+                  let newLi = ";"
+                  if(gpib == false){
+                      newLi = "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + model + "</a></li>";
+                  }
+                  if(gpib == true){
+                      newLi = "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + model + "</a>" + span + "</li>";
+                  }
+                  $("#ul" + sku).append(newLi);
             }
         });
     });
