54e30cb33419e80e336891de2e72634c5ecfdf94
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-08 15:58:05 -0500

    I have no idea why this doesn't work, as usual

--- js/reserve.js
+++ js/reserve.js
@@ -68,8 +68,7 @@ function equipmentList(){
                   $("#optgroup" + equipmentData.sku).append(newEquipment);
               }
               $("#option" + equipment.key).click(function(){
-                  let keyString = [];
-                  keyString[0] = (equipment.key).toString();
+                  let keyString = equipment.key.toString();
                   equipment.push(keyString);
                   $("#option" + equipment.key).prop("data-submisison", keyString);
                   $("#option" + equipment.key).addClass("submisison");
