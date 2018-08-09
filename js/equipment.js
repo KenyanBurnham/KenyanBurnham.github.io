e641d1be2ce8e6b32c5d7dd2e266d55aa5ac91b1
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-09 11:41:00 -0500

    I wonder why

--- js/equipment.js
+++ js/equipment.js
@@ -2,6 +2,7 @@ function generatePage(){
     let span = "<span class='glyphicon glyphicon-ok-circle'></span>";
     firebase.database().ref("equipment").once("value").then(function(equipment){
         equipment.forEach(function(item){
+
             let equipmentKey = item.key;
             let itemData = item.val();
             let model = itemData.model;
@@ -10,13 +11,12 @@ function generatePage(){
             let sku = itemData.sku;
             let url = itemData.url;
             let gpib = itemData.gpib;
+            let category = itemData.category;
             let skuCheck = $("#" + sku).hasClass("sku");
+            let bookmark = "<li><a href='#" + sku +"' class='make-block-item nav-sub'>" + type + "</a></li>";
             if(skuCheck == false){
-                console.log("sku Check false:" +  skuCheck);
-                let bookmark = "<li><a href='#" + sku +"' class='make-block-item nav-sub'>" + type + "</a></li>"
                 $("#bookmarkLinks").append(bookmark);
                 if(gpib == false){
-                    console.log("gpib  false:" +  gpib);
                     let newRow = "<div class='row'><div class='col-2'></div><div class='col-2'>" +
                           "<h4 id = '" + sku + "' class = 'title'>" + type + "</h4><ul id='ul" + sku + "'>" +
                               "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + " " + model + "</a></li>" +
@@ -24,7 +24,6 @@ function generatePage(){
                     $("#benchTopEquipment").append(newRow);
                 }
                 if(gpib == true){
-                  console.log("gpib  true:" +  gpib);
                     let newRow2 = "<div class='row'><div class='col-2'></div><div class='col-2'>" +
                           "<h4 id = '" + sku + "' class = 'title'>" + type + "</h4><ul id='ul" + sku + "'>" +
                               "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + " " + model + "</a>" + span + "</li>" +
@@ -32,14 +31,11 @@ function generatePage(){
                     $("#benchTopEquipment").append(newRow2);
                 }
             }else if(skuCheck == true){
-                  console.log("skuCheck  false:" +  skuCheck);
                   let newLi = "";
                   if(gpib == false){
-                      console.log("gpib  false:" +  gpib);
                       newLi = "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + " " + model + "</a></li>";
                   }
                   if(gpib == true){
-                      console.log("gpib  true:" +  gpib);
                       newLi = "<li id='li" + equipmentKey + "'> <a href='" + url + "'>" + manufacturer + " " + model + "</a>" + span + "</li>";
                   }
                   $("#ul" + sku).append(newLi);
