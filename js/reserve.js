89500e609750bb205f4881188b252efadec447be
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-13 13:52:33 -0500

    Helps, but doesn't fix everything

--- js/reserve.js
+++ js/reserve.js
@@ -313,22 +313,9 @@ $( document ).ready(function() {
       //https://codepen.io/kwsim/pen/xqNpLQ
       //Kenneth Sim
       $('.btn-toggle').click(function() {
-          $(this).find('.btn').toggleClass('active');
-
-          if ($(this).find('.btn-primary').length>0) {
-              $(this).find('.btn').toggleClass('btn-primary');
-          }
-          if ($(this).find('.btn-danger').length>0) {
-              $(this).find('.btn').toggleClass('btn-danger');
-          }
-          if ($(this).find('.btn-success').length>0) {
-              $(this).find('.btn').toggleClass('btn-success');
-          }
-          if ($(this).find('.btn-info').length>0) {
-              $(this).find('.btn').toggleClass('btn-info');
-          }
-          $(this).find('.btn').toggleClass('btn-default');
-          //Calls after css change to make sure table gets updated
+          $(this).find('#btn1').toggleClass('active');
+          $(this).find('#btn2').toggleClass('active');
+          
           if($(".is-selected").hasClass("active")){
               fillTable(".is-selected");
           }
