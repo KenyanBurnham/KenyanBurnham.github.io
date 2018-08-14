c79a4588d4a6c372306285da3d4cd823c99f12ff
Kenyan Burnham <kenyan.burnham@ttu.edu>
2018-08-14 16:33:56 -0500

    I feel like this does the same thing, but okay

--- js/reserve.js
+++ js/reserve.js
@@ -255,14 +255,7 @@ function filter(timeStamp){
                         $("#span" + timeStamp).text("" + equipmentCounter + "");
                         $("#span" + timeStamp).prop("hidden", false);
                         $("tr[data-timestamp='" + timeStamp + "']").click(function(){
-                            if ($("tr[data-timestamp='" + timeStamp + "']").hasClass("chosen")){
-                                $("tr[data-timestamp='" + timeStamp + "']").removeClass("chosen");
-                                //remove from time reservation
-                            } else {
-                                $("tr[data-timestamp='" + timeStamp + "']").addClass("chosen");
-                                viewEquipment(timeStamp);
-                                //add time to reservation data
-                            }
+                            viewEquipment(timeStamp);
                         });
                     }
               }
